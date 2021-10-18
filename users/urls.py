f5d2a1b77eaf5886f7eee0e4d9601a5c52a380e9
Taufik Pragusga <pragusga25@gmail.com>
2021-10-18 18:13:48 +0700

    create login form

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.profile, name="profile"),
     path("edit-profile", views.edit_profile, name="edit-profile"),
+    path("login", views.login, name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -11,7 +11,8 @@ def profile(request):
 
 
 def edit_profile(request):
-    # profile = Profile.objects.get(user=request.user)
-    # profile = Profile.objects.get(id="00235683-df81-4715-9b74-efcf81c16d92")
-    # context = {"profile": profile}
     return render(request, "users/edit-profile.html")
+
+
+def login(request):
+    return render(request, "users/login.html")
