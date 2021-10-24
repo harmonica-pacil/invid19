6d808b5c22555f0551a6357effbdc06dae457cf1
gitJason <jason.widodo@ui.ac.id>
2021-10-24 23:03:09 +0700

    1

---
+++ artikel/views.py
@@ -0,0 +1,20 @@
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate, logout
+
+
+# Create your views here.
+# def artikel(request):
+#     return render(request, "html")
+
+# @login_required(login_url="login")
+# def buat_Artikel(request):
+#     form = 
+
+#     if request.method == "POST":
+#         form =
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Artikel was updated!")
+#             return redirect("artikel")
+
