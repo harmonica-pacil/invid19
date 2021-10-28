b48f275ef79e773a3a3fbbcbd032ef980ffc455a
haddad <emha.haddad@gmail.com>
2021-10-28 23:18:04 +0700

    merge

--- diskusi/urls.py
+++ diskusi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import index, add_forum, index_json
+from comment.views import index as comment_index
 
 urlpatterns = [
     path('', index, name='index'),
     path('add/', add_forum, name='add'),
     path('json/', index_json, name='json api'),
-    path('<int:id>', index, name='comment'),
+    path('<int:id>', comment_index, name='comment'),
 ]
