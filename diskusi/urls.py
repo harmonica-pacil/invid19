886458a68ebe47a39acc658bc762f1e5a463ca45
haddad <emha.haddad@gmail.com>
2021-10-28 23:41:43 +0700

    Revert "merge"
    This reverts commit b48f275ef79e773a3a3fbbcbd032ef980ffc455a.
    
    revert

--- diskusi/urls.py
+++ diskusi/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from .views import index, add_forum, index_json
-from comment.views import index as comment_index
 
 urlpatterns = [
     path('', index, name='index'),
     path('add/', add_forum, name='add'),
     path('json/', index_json, name='json api'),
-    path('<int:id>', comment_index, name='comment'),
+    path('<int:id>', index, name='comment'),
 ]
