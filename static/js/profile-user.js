1d22af325b665ea9b3187a6cef18bcf37c4111b1
Taufik Pragusga <pragusga25@gmail.com>
2021-10-29 14:34:08 +0700

    using jquery

--- static/js/profile-user.js
+++ static/js/profile-user.js
@@ -1,42 +1,50 @@
-const displayPage = () => {
-  document.querySelector('#loader-container').style.display = 'none';
-  document.querySelector('#root').style.display = 'block';
-};
+$(document).ready(() => {
+  const showPage = () => {
+    $('#loader-container').css('display', 'none');
+    $('#root').css('display', 'block');
+  };
 
-const hidePage = () => {
-  document.querySelector('#loader-container').style.display = 'flex';
-  document.querySelector('#root').style.display = 'none';
-};
+  const hidePage = () => {
+    $('#loader-container').css('display', 'flex');
+    $('#root').css('display', 'none');
+  };
 
-hidePage();
+  hidePage();
 
-const username = document.getElementById('profile-username');
-const intro = document.getElementById('profile-intro');
-const fullname = document.getElementById('profile-fullname');
-const email = document.getElementById('profile-email');
-const bio = document.getElementById('profile-bio');
-const address = document.getElementById('profile-address');
-const image = document.getElementById('profile-image');
+  const username = $('#profile-username');
+  const intro = $('#profile-intro');
+  const fullname = $('#profile-fullname');
+  const email = $('#profile-email');
+  const bio = $('#profile-bio');
+  const address = $('#profile-address');
+  const image = $('#profile-image');
 
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    const res = JSON.parse(this.responseText)[0].fields;
-    username.innerHTML = res.username;
-    intro.innerHTML = res.short_intro;
-    fullname.innerHTML = res.name;
-    email.innerHTML = res.email;
-    bio.innerHTML = res.bio;
-    address.innerHTML = res.address;
-    // profiles/default-user_pfzkxt
-    if (res.profile_image === 'profiles/default-user_pfzkxt') {
-      image.src = `https://res.cloudinary.com/da66vxlpb/image/upload/v1/images/media/${res.profile_image}`;
-    } else {
-      image.src = `https://res.cloudinary.com/da66vxlpb/image/upload/v1/${res.profile_image}`;
-    }
-    image.alt = res.name;
-    setTimeout(displayPage, 2500);
-  }
-};
-xhttp.open('GET', '/user/profile/json', true);
-xhttp.send();
+  $.ajax({
+    url: '/user/profile/json',
+    success: function (result) {
+      const res = result[0].fields;
+
+      username.text(res.username);
+      intro.text(res.short_intro);
+      fullname.text(res.name);
+      email.text(res.email);
+      bio.text(res.bio);
+      address.text(res.address);
+      // profiles/default-user_pfzkxt
+      if (res.profile_image === 'profiles/default-user_pfzkxt') {
+        image.attr(
+          'src',
+          `https://res.cloudinary.com/da66vxlpb/image/upload/v1/images/media/${res.profile_image}`
+        );
+      } else {
+        image.attr(
+          'src',
+          `https://res.cloudinary.com/da66vxlpb/image/upload/v1/${res.profile_image}`
+        );
+      }
+      image.attr('alt', res.name);
+
+      showPage();
+    },
+  });
+});
