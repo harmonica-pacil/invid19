2842a74c3f125498a3312eaf16cb077975c225ab
@irhamkuro92 <irhamkuro92@gmail.com>
2021-10-30 22:19:12 +0700

    Fix diskusi photo and placement

--- static/js/forum_list.js
+++ static/js/forum_list.js
@@ -16,7 +16,7 @@ $(function (){
                 }else{
                     forum['creator_image'] = 'https://res.cloudinary.com/da66vxlpb/image/upload/v1/' + forum['creator_image'];
                 }
-                $stack.append('<div class="card" id = "' + dict['pk'] + '" onClick="reply_click(this.id)"><div class="card-body"><p class="h4">' + forum['title'] + '</p><div class="user"><div class="d-flex flex-column"><div class="container"><div class = "image_user"><img src="' + forum['creator_image'] + '" class="rounded float-start" alt=""></div><p class="h6 text-muted">' + forum['creator_username'] + ' - ' + forum['created_at'] + '</p></div><div class="container"><p class="deskripsi">' + forum['message'] + '</p></div></div></div></div></div>');
+                $stack.append('<div class="card" id = "' + dict['pk'] + '" onClick="reply_click(this.id)"><div class="card-body"><p class="h4">' + forum['title'] + '</p><div class="user"><div class="d-flex flex-column"><div class="container"><div class = "image_user"><img src="' + forum['creator_image'] + '" class="float-start" style = "margin-right: 10px; object-fit: fill;  border-radius: 50%;" width="30" height="30" alt=""></div><p class="h6 text-muted" style="padding-top: 3px">' + forum['creator_username'] + ' - ' + forum['created_at'] + '</p></div><div class="container"><p class="deskripsi">' + forum['message'] + '</p></div></div></div></div></div>');
             });
         }
     });
