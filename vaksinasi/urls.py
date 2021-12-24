c480fafca5da07098448fc26bdc1efbd0e045453
rickyantowm <rickyanto.wangsa@ui.ac.id>
2021-12-24 14:12:32 +0700

    setup

--- vaksinasi/urls.py
+++ vaksinasi/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('add-vaksin', views.add_vaksin, name='add-vaksin'),
     path('add-pendaftar', views.add_pendaftar, name='add-pendaftar'),
-    path('lihat-vaksin', views.index, name='lihat-vaksin'),
+    path('', views.index, name='lihat-vaksin'),
     path('lihat-individu', views.lihat_individu, name='lihat-individu'),
     path('load', views.load, name='load'),
+    path('json', views.index_json, name='json'),
+    path('add-pendaftar-flutter', views.add_pendaftar_flutter, name='add-pendaftar-flutter'),
+    path('add-vaksin-flutter', views.add_vaksin_flutter, name='add-vaksin-flutter'),
 ]
--- vaksinasi/views.py
+++ vaksinasi/views.py
@@ -6,6 +6,8 @@ from django.core.paginator import Paginator
 from django.core import serializers
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     if 'q' in request.GET:
@@ -18,6 +20,15 @@ def index(request):
     page_number=request.GET.get('page')
     posts_obj=paginator.get_page(page_number)
     return render(request,'index.html',{'vaksins':posts_obj})
+
+def index_json(request):
+    if 'q' in request.GET:
+        q=request.GET['q']
+        vaksins=Vaksin.objects.filter(title__icontains=q)
+    else:
+        vaksins=Vaksin.objects.order_by('tanggal')
+    data = serializers.serialize('json', vaksins)
+    return HttpResponse(data, content_type="application/json")
 
 @login_required(login_url="login")
 def lihat_individu(request):
@@ -50,6 +61,58 @@ def add_pendaftar(request):
     else:
         form = PendaftarForm()
     return render(request, 'form_daftar.html', {'form': request.POST['kode']})
+
+@csrf_exempt
+def add_pendaftar_flutter(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        kode = data["kode"]
+        NIK = data["NIK"]
+        nama_lengkap = data["nama_lengkap"]
+
+        form = PendaftarForm(
+            kode = kode,
+            NIK = NIK,
+            nama_lengkap = nama_lengkap
+        )
+        
+        form.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+@csrf_exempt
+def add_vaksin_flutter(request):
+    if request.method == 'POST':
+        kode = data["kode"]
+        kota = data["kota"]
+        provinsi = data["provinsi"]
+        lokasi = data["lokasi"]
+        jenis_vaksin = data["jenis_vaksin"]
+        tanggal = data["tanggal"]
+        jam_mulai = data["jam_mulai"]
+        jam_berakhir = data["jam_berakhir"]
+        kuota = data["kuota"]
+
+        form = VaksinForm(
+            kode = kode,
+            kota = kota,
+            provinsi = provinsi,
+            lokasi = lokasi,
+            jenis_vaksin = jenis_vaksin,
+            tanggal = tanggal,
+            jam_mulai = jam_mulai,
+            jam_berakhir = jam_berakhir,
+            kuota = kuota
+        )
+        
+        form.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
 
 def load(request):
     offset=int(request.POST['offset'])
